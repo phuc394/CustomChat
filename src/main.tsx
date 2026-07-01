0e15399c6a0c89c253daa1fc2507258b9e4c60a0
phuc394 <phuc.thg02394@sinhvien.hoasen.edu.vn>
2026-07-01 15:08:56 +0700

    feat: Created a naviagtion feature by using React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
+    <RouterProvider router={router} />
   </StrictMode>
 )
